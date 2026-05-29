9448d57b233faa92f42c6950a972da349cc271c7
Cesar Nunez <126050501+rasec100@users.noreply.github.com>
2026-05-29 10:16:16 -0500

    Paginas cesar (#2)
    * agregando paginas de tareas y cursos + mejora dashboard
    
    Co-Authored-By: rasec100 <cesar@nunez.pe>
    
    * fix typo en nombre de archivo inde.tsx -> index.tsx
    
    Co-Authored-By: rasec100 <cesar@nunez.pe>
    
    * agregando informe de contribucion
    
    Co-Authored-By: rasec100 <cesar@nunez.pe>
    
    * removing informe del repo
    
    Co-Authored-By: rasec100 <cesar@nunez.pe>
    
    ---------
    
    Co-authored-by: rasec100 <cesar@nunez.pe>

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,7 +1,10 @@
 import { createBrowserRouter } from 'react-router';
 import NotFound from '@/pages/NotFound';
-import DashBoardPage from '@/pages/dashboard/inde';
+import DashBoardPage from '@/pages/dashboard';
+import TasksPage from '@/pages/tasks';
+import CoursesPage from '@/pages/courses';
 import AppLayout from '@/app/layouts/AppLayout';
+
 export default createBrowserRouter([
   {
     Component: AppLayout,
@@ -9,6 +12,14 @@ export default createBrowserRouter([
       {
         index: true,
         Component: DashBoardPage,
+      },
+      {
+        path: 'tasks',
+        Component: TasksPage,
+      },
+      {
+        path: 'courses',
+        Component: CoursesPage,
       },
     ],
   },
